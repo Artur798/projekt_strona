d2bc4e97a25c0bc9ec1cf90977cf938f5854ee1e
Artur798 <Artur798@github.com>
2021-09-12 21:35:45 +0200

    update

--- pogoda.js
+++ pogoda.js
@@ -8,9 +8,8 @@ const getWeatherForCity = async(city) => {
      }})
    .then(response => response.json())
    .then(data => {
-     console.log('data',data);
+   
 	 var dane=data[city];
-   console.log(dane);
    document.getElementById("temperatura").innerHTML= "temperatura: " + dane.temperatura + "°C";
    document.getElementById("wiatr").innerHTML= "prędkość wiatru: " + dane.wiatr + "km/h";
    
