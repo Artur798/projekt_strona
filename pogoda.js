aa6a588e1e12eb9534558ec043784bb6eb47241e
Artur798 <Artur798@github.com>
2021-09-12 21:27:52 +0200

    update

--- pogoda.js
+++ pogoda.js
@@ -1,7 +1,7 @@
 const getWeatherForCity = async(city) => {
 	//var city = document.getElementById("city").value;
 	
-    fetch('https://Artur798.github.io/projekt123/pogdan.json', {
+    fetch('https://Artur798.github.io/projekt_strona/pogdan.json', {
      method:'GET',
      headers: {
        'Content-Type': 'application/json',
@@ -11,8 +11,8 @@ const getWeatherForCity = async(city) => {
      console.log('data',data);
 	 var dane=data[city];
    console.log(dane);
-   document.getElementById("temperatura").innerHTML= "temperatura: " + dane.temperatura;
-   document.getElementById("wiatr").innerHTML= "wiatr: " + dane.wiatr;
+   document.getElementById("temperatura").innerHTML= "temperatura: " + dane.temperatura + "°C";
+   document.getElementById("wiatr").innerHTML= "prędkość wiatru: " + dane.wiatr + "km/h";
    
    })
    .catch((err) => {
